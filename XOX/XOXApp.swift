058656792f792fcf64589ee87d2ba76dd540c294
dexter ramos <blitzdex27@gmail.com>
2023-10-09 21:21:04 +0800

    Initial Commit

---
+++ XOX/XOXApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOXApp.swift
+//  XOX
+//
+//  Created by Dexter Ramos on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct XOXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
